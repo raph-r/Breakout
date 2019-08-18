71b89e701f73131c017868d20a8da12fcb6ab803
rrtf <raphaelrtf@gmail.com>
2019-08-18 14:56:02 -0300

    Add constants

--- Breakout/Constant.cpp
+++ Breakout/Constant.cpp
@@ -6,3 +6,8 @@ const unsigned int Constant::HALF_SCREEN_WIDTH = Constant::SCREEN_WIDTH / 2;
 const unsigned int Constant::HALF_SCREEN_HEIGHT = Constant::SCREEN_HEIGHT / 2;
 const unsigned int Constant::KEY_SEEN = 1;
 const unsigned int Constant::KEY_RELEASED = 2;
+
+const unsigned int Constant::AMOUNT_OF_BLOCK_ALIGN_VERTICALLY = 6;
+const unsigned int Constant::AMOUNT_OF_BLOCK_ALIGN_HORIZONTALLY = 18;
+const unsigned int Constant::BLOCK_WIDTH = 24;
+const unsigned int Constant::BLOCK_HEIGHT = 15;
--- Breakout/Constant.h
+++ Breakout/Constant.h
@@ -16,5 +16,10 @@ class Constant
 		static const unsigned int HALF_SCREEN_HEIGHT;
 		static const unsigned int KEY_SEEN;
 		static const unsigned int KEY_RELEASED;
+
+		static const unsigned int AMOUNT_OF_BLOCK_ALIGN_VERTICALLY;
+		static const unsigned int AMOUNT_OF_BLOCK_ALIGN_HORIZONTALLY;
+		static const unsigned int BLOCK_WIDTH;
+		static const unsigned int BLOCK_HEIGHT;
 };
 
