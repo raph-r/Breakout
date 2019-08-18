ad000ee938adad10c878d6ea9cc6158a2116ae30
rrtf <raphaelrtf@gmail.com>
2019-08-18 19:00:52 -0300

    Removes unused method and fix identify_pressed_key

--- Breakout/Util.cpp
+++ Breakout/Util.cpp
@@ -1,25 +1,8 @@
 #include "Util.h"
 
-bool  Util::is_horizontal(const unsigned int & direction)
-{
-	if (direction == ALLEGRO_KEY_LEFT || direction == ALLEGRO_KEY_RIGHT)
-	{
-		return true;
-	}
-	return false;
-}
-
 unsigned int Util::identify_pressed_key(const unsigned char * key)
 {
-	if (key[ALLEGRO_KEY_UP])
-	{
-		return ALLEGRO_KEY_UP;
-	}
-	else if (key[ALLEGRO_KEY_DOWN])
-	{
-		return ALLEGRO_KEY_DOWN;
-	}
-	else if (key[ALLEGRO_KEY_LEFT])
+	if (key[ALLEGRO_KEY_LEFT])
 	{
 		return ALLEGRO_KEY_LEFT;
 	}
